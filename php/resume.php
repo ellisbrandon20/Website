97789c5bfc39e1484fbdfb8e594628a3f7adcd8a
ellisbrandon20 <ellisbrandon20@tamu.edu>
2016-12-22 01:38:12 -0600

    update resume to mobile friendly

--- php/resume.php
+++ php/resume.php
@@ -4,143 +4,82 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title> Brandon Ellis E-Portfolio Resume</title>
-        <link rel="icon" type="image/png" href="/pics/title_icon.png">
-		<link rel="stylesheet" type="text/css" href="/css/style2.css">	
-    </head>
-   
-    <body bgcolor="#f2f2f2">
-        <div class = "header">
-            <div class="headerBreak"></div>
-            <div class = "headerContent">
-                <center><img src="/pics/header_2_resume.png" alt="Resume Header image" title="Resume Header image"</center>
-                <div class = "nav">
-                    <?php include('../includes/nav.php');?>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--        <link rel="icon" type="image/png" href="/pics/title_icon.png">-->
+<!--		<link rel="stylesheet" type="text/css" href="/css/style2.css">-->
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="/css/navbar.css">
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+        
+        
+        <title> Brandon Ellis E-Portfolio Resume</title>
+        
+	</head>
+
+    <body>
+        <!-- Nav bar -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid navbar-container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand" href="/php/index.php">Brandon-Ellis.Net</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">            
+                    <ul class="nav navbar-nav">
+                        <?php //include('../includes/nav.php');?>
+                        <li><a href="/php/index.php">Home</a></li>
+                        <li class="active"><a href="/php/resume.php">Resume</a></li>
+                        <li><a href="/php/gallery.php">Gallery</a></li>
+                        <li><a href="/php/projects.php">Projects</a></li>
+                    </ul>
                 </div>
             </div>
-            <div class = "headerBreak2"></div>
+        </nav>
+
+        <div class="bgcolor">
+        <!-- Header image -->
+        <div class="header-img">
+            <center><img class="img-responsive" src="/pics/header_2_resume.png" alt="Header image" width="800" height="200"></center>
         </div>
-    <div class="body">
-        <div class = "outercontainer">
-            <!--links to resume-->
-           <!-- <center>
-                You can view my Resume in .pdf and .docx formats.
-            </center>
-            <center>
-                <a href="/cgi-bin/Resume_Ellis_Brandon_Spring2016.pdf" target="_blank">Click Here</a> for .pdf format
-                 or <a href="/cgi-bin/Resume_Ellis_Brandon_Spring2016.docx" target="_blank">Click Here</a> for .docx format
-            </center>
-             end of common code
-            <center>
-                <br><font size="10">Brandon Ellis</font>
-                <br>Permanent Address: 107 Mariner Drive, Victoria, Texas 77901
-                <br>Primary Email:<a href="mailto:ellis.brandon2017@gmail.com">ellis.brandon2017@gmail.com</a>
-                <br>School Email:<a href="mailto:ellisbrandon20@tamu.edu">ellisbrandon20@tamu.edu</a>
-                <br>Cell Phone: (361) 652-8692
-            </center>
-            <p>
-                <b><u><font size="5">Objective</font></b></u><br>
-                <br>Seeking an internship that will lead to opportunities in UI development, Mobile Systems, and Web Development
-            </p>
-            <p>
-                <b><u><font size="5">Education</font></b></u><br>
-                <br><u>Texas A&#38;M University - College Station, TX</u><i>&nbsp;&nbsp;&nbsp;(class of 2017)</i>
-                <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Bachelor of Science in Computer Science
-                <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Overall GPR: 3.41
-                <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Major GPR: 3.69
-                <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Expected Graduation: Dec. 2017
-            </p>
-            <p>
-                <b><u><font size="5">Honors</font></b></u><br>
-                <ul>
-                    <li><u>Distinguished Student Honor Roll</u>: Spring 2014, Fall 2014, Spring 2015</li>
-                    <li><u>Top 10% Scholarship</u>: Fall '13, 14, '15</li>
-                </ul>
-            </p>
-            <p>
-            <p>
-                <b><u><font size="5">Technical Skills</font></b></u>
-                <ul>
-                    <li><b>Languages:</b> C++ (Strong understanding), Java, SQL (MS Access), Visual Basic</li>
-                    <li><b>Web:</b> HTML, CSS (Basic), PHP(currently learning)</li>
-                    <li>Familiarity of Windows and MAC OS (Xcode, Notepad++, Visual Studio, Unix/Linux with g++ 4.9)</li>
-                </ul>
-            </p>
-                <b><u><font size="5">Personal Projects</font></b></u>
-                <ul>
-                    <li><b>Peronsal Webpage</b>:  brandon-ellis.net; using HTML, CSS, and PHP to create a personal website that has a small autobiography, resume files, and gallery. Currently implementing a virtual cook book page which can be accessed with a correct password.</li>
-                    <li><b>Interactive Slide Show</b>: Using VBA, create a UI that can add/delete songs and bible verses to MS PPT slide show while in running presentation, and to a list box so you know what content is on each slide.</li>
-                    <li><b>Chance</b>: A dice gambling game using C++ includes a log in system and high score.</li>
-                    <li><b>IOS</b>: currently learning ios app development in spare time outside of coursework.</li>
-                </ul>
-            </p>
-            <p>
-                <b><u><font size="5">Work Experience</font></b></u><br>
-                <br>
-                <u>4.0 and Go Test Review and Tutoring, College Station, TX</u>&nbsp;&nbsp;&nbsp;<i>(January '15-August '15)</i>
-                <br><i>Programmer</i>
-                <ul>
-                    <li>Created a Clock In/Out System, includes a username and hidden password UI, that interacts with a database of all
-                        entries</li>
-                    <li>Contributed to website adjustments and ideas</li>
-                    <li>Contributed to bug fixes in programs</li>
-                </ul>
 
-                <u>Victoria College / Museum of the Coastal Bend, Victoria, TX</u>&nbsp;&nbsp;&nbsp;<i>(March '13-May'13, and June '14-July '14)</i>
-                <br><i>Student Worker</i>
-                <ul>
-                    <li>Guided guests into the museum and operated the cash register for entrance fees and gift store purchases</li>
-                    <li>Cleaned the museum display cases before the museum opened and after the museum opened</li>
-                    <li>At the Leo J. Welder Center, I worked at the Snacks Bar where I sold snacks and bartended before shows</li>
-                </ul>
+        <!-- conatiner content -->
+            <div class="container body-content">
+                <object>
+                    <embed src="../data/Resume_Ellis_Brandon_Summer_2016.pdf" width="100%" height="400px"/>
+                </object>
+                
+                <center><button type="button" class="btn btn-default" style="margin-top:2em" onclick="openResumePDF()">View PDF</button></center>
 
-            </p>
-            <p>
-                <b><u><font size="5">Organizations</font></b></u><br>
+                <!-- footer -->
+                <br/>
+                <hr class="double-line"/>
 
-                <br><u>UPE</u> - Upsilon Pi Epsilon &nbsp;&nbsp;&nbsp;<i>(August '15 - Present)</i>
-                <br><i>Webmaster</i>
-                <ul>
-                    <li>Maintain website and all social websites associated with UPE to be sure events are up to date and available to other members</li>
-                </ul>
-
-                <br><u>TAMU Rotaract</u> &nbsp;&nbsp;&nbsp;<i>(August '15 - Present)</i>
-                <br><i>General Member</i>
-                <ul>
-                    <li>Work numerous volunteer events in the Bryan-College Station area to give thanks back to community</li>
-                </ul>
-
-                <br><u>TACS</u> - Texas A&#38;M Computing Society &nbsp;&nbsp;&nbsp;<i>(January '15-Present)</i>
-                <br><i>General Member</i>
-                <ul>
-                    <li>Attended networking meetings to meet and engage with other professionals</li>
-                </ul>
-            </p>
-            <p>
-                <b><u><font size="5">Volunteer Work</font></b></u><br>
-                <ul>
-                <li><u>The Big Event</u>, Texas A&#38;M University &nbsp;&nbsp;&nbsp;<i>(Spring of '14, '15)</i></li>
-                    <ul>
-                        <li>Worked for an elderly couple to assist with cleaning and maintaining their property</li>
-                    </ul>
-
-                <li><u>Basic Training Camp</u>, First United Methodist Church, Victoria, TX &nbsp;&nbsp;&nbsp;<i>(Summers of '09-'12)</i></li>
-                    <ul>
-                        <li>Served as a small group leader for 8-12 young 3<sup>rd</sup> - 5<sup>th</sup> graders</li>
-                    </ul>
-                </ul>
-            </p>
--->
-
-            <iframe class="resumeDoc" src="../data/Resume_Ellis_Brandon_Summer_2016.pdf" width="100%" height="100%"></iframe>
-
-            <!--footer -->
-            <div class = "footer">
-                 <hr width="500"/>
-<!--                <center><div style="width:100px;height:25px;border:1px solid #000;">Contact Me</div></center>-->
+                <!-- social media external links -->
+                <center>
+                    <table>
+                        <tr>
+                            <?php include('../includes/footer.php'); ?>
+                        </tr>
+                    </table>
+                </center>
             </div>
-            <!-- end footer -->
-        </div>
-    </div>
+        </div>        
     </body>
-</html>
+</hmtl>
+      
+<script>
+function openResumePDF() {
+    location.href = "http://brandon-ellis.net/data/Resume_Ellis_Brandon_Summer_2016.pdf";
+}
+</script>
